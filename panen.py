de64a38c3c4e795f3da2e26e6a1825313d5142ee
Tiara Quranique <tiaraq0812@gmail.com>
2026-09-18 18:03:32 +0700

    Menambahkan fitur diskon

--- panen.py
+++ panen.py
@@ -2,9 +2,19 @@ def hitung_total_panen(jumlah_karung, berat_per_karung):
     total = jumlah_karung * berat_per_karung
     return total
 
+def hitung_diskon(total_harga, persen_diskon):
+    potongan = total_harga * persen_diskon / 100
+    harga_akhir = total_harga - potongan
+    return harga_akhir
+
 jumlah_karung = 12
 berat_per_karung = 25
+harga_per_kg = 8000
 
 total_panen = hitung_total_panen(jumlah_karung, berat_per_karung)
+total_harga = total_panen * harga_per_kg
+harga_akhir = hitung_diskon(total_harga, 5)
 
 print("Total hasil panen :", total_panen, "kg")
+print("Total harga : Rp", total_harga)
+print("Harga setelah diskon : Rp", harga_akhir)
